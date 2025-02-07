f3c6c09fdd8ce0599d6c7dafab7f3d575e7b7d37
seif1125 <seifammar1125@gmail.com>
2025-02-07 15:26:16 +0200

    Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>⌚ Smartwatch Showcase</h1>
+     <p>Building step by step...</p>
+    </div>
+  );
+}
+
+export default App;
